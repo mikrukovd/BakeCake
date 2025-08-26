0bf64f1786a732095614776a1b09c7ecf8a3be97
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-08-27 00:52:52 +0500

    Написал более подробный шаблон обработки ввода промокода

--- handlers/message_handlers.py
+++ handlers/message_handlers.py
@@ -3,10 +3,16 @@ from . import states_bot
 
 
 
-async def validate_text(update, context):
-    await update.message.reply_text(
-        "введите текст",
-        reply_markup=keyboard. # тут нужная клавиатура
-    )
-    # тут обработка
-    return states_bot.INPUT_PHONE
+async def validate_promocode(update, context):
+    if True: #тут вместо True проверка промокода на действительность
+        await update.message.reply_text(
+            "Промокод активирован!",
+            reply_markup=keyboard. # тут нужная клавиатура для состояния STATE_2
+        )
+        return states_bot.STATE_2 # состояние 2
+    else:
+        await update.message.reply_text(
+            "Промокод не подошел!",
+            reply_markup=keyboard. # тут нужная клавиатура для нашего сотояния STATE_1
+        )
+        return states_bot.STATE_1 # Наше текущее состояние(тоесть при неправильном промокоде мы вновь оказываемя тут же и снова вводим промокод)
