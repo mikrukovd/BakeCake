9191363c0484ad66400cc1afa4aab76b59763781
mikrukovd <z123456689z@gmail.com>
2025-08-28 10:47:22 +0300

    Добавил состояния

--- handlers/states_bot.py
+++ handlers/states_bot.py
@@ -1,1 +1,26 @@
-(STATE_1, STATE_2, STATE_3) = range(3) # состояния в которых находиться пользователь
+(
+    STATE_PPD,                    # Согласие с ПД
+    STATE_MAIN_MENU,              # Главное меню
+    STATE_SIMPLE_ORDER,           # Выбор готового торта
+    STATE_CUSTOM_ORDER,           # Начало кастомного заказа
+    STATE_LEVEL_SELECTION,        # Выбор уровня торта
+    STATE_FORM_SELECTION,         # Выбор формы торта
+    STATE_TOPPING_SELECTION,      # Выбор топпинга
+    STATE_BERRIES_OPTION,         # Опция добавления ягод
+    STATE_BERRIES_SELECTION,      # Выбор ягод
+    STATE_DECOR_OPTION,           # Опция добавления декора
+    STATE_DECOR_SELECTION,        # Выбор декора
+    STATE_CAPTION_OPTION,         # Опция добавления надписи
+    STATE_CAPTION_INPUT,          # Ввод надписи
+    STATE_COMMENT_OPTION,         # Опция добавления комментария
+    STATE_COMMENT_INPUT,          # Ввод комментария
+    STATE_DELIVERY_INFO,          # Информация о доставке
+    STATE_DELIVERY_TIME,          # Выбор времени доставки
+    STATE_DELIVERY_ADDRESS,       # Ввод адреса доставки
+    STATE_PROMO_CODE,             # Ввод промокода
+    STATE_CONFIRM_ORDER,          # Подтверждение заказа
+    STATE_PRICE_INFO,             # Информация о ценах
+    STATE_PAYMENT_METHOD,         # Выбор способа оплаты
+    STATE_ORDER_REVIEW,           # Просмотр и редактирование заказа
+    STATE_ORDER_CANCEL            # Отмена заказа
+) = range(24)
