61c17a890319fe582ce3399bbe09b5ee6a5f714f
darya-ko00 <dasha.kon.060203@gmail.com>
2025-09-02 17:53:35 +0300

    функция для валидации номера

--- ptb/handlers/message_handlers.py
+++ ptb/handlers/message_handlers.py
@@ -1,6 +1,23 @@
 # from keyboards import keyboard # нужны клавиатуры
 # from . import states_bot
+# import re 
 
+async def validate_phone(update, context):
+	phone_number = update.message.text.strip()
+    phone_pattern = re.compile(r'^[+]?[0-9]{0,3}W?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$', re.IGNORECASE)
+    
+    if phone_pattern.match(phone_number):
+        await update.message.reply_text(
+            "Номер телефона валиден!",
+            reply_markup=keyboard.STATE_2
+        )
+        return states_bot.STATE_2 
+    else:
+        await update.message.reply_text(
+            "Номер телефона указан неверно.Пожалуйста, введите корректный номер.",
+            reply_markup=keyboard.STATE_1
+        )
+        return states_bot.STATE_1 
 
 
 # async def validate_promocode(update, context):
